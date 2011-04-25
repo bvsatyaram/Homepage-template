f5a82040ce1d3a0c528717af8fdbdef8e52075fc
B V Satyaram <bvsatyaram@gmail.com>
2011-04-26 02:25:01 +0530

    Thinner header

--- application.js
+++ application.js
@@ -1,36 +1,44 @@
 ///////////////////
 // Navigation Tab
 /////////////////
-$(function() {
+$(document).ready(function(){
   $( "ul#topnav li a.unselected" ).hover(
     function() {
-      $( this ).animate({
+      $(this).stop().animate({
         backgroundColor: "#505050"
       }, {
         queue:false,
         duration:500
-      } );
-      return false;
+      });
     },
     function() {
-      $( this ).animate({
+      $(this).stop().animate({
         backgroundColor: "#2E2E2E"
       }, {
         queue:false,
         duration:500
-      } );
-      return false;
+      });
     });
-})
+});
 
 
 ///////////////////
 // Welcome Actions
 ///////////////////
 $(document).ready(function(){
-	$('#welcome_actions .action').hover(function(){
-		$(".caption", this).stop().animate({top:'0px'},{queue:false,duration:300});
-	}, function() {
-		$(".caption", this).stop().animate({top:'-40px'},{queue:false,duration:300});
-	});
+  $('#welcome_actions .action').hover(function(){
+    $(".caption", this).stop().animate({
+      top:'0px'
+    },{
+      queue:false,
+      duration:300
+    });
+  }, function() {
+    $(".caption", this).stop().animate({
+      top:'-40px'
+    },{
+      queue:false,
+      duration:300
+    });
+  });
 });
